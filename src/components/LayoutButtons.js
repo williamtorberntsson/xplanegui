f03da163fc50cdc299073b18f98e245e5b004817
wilto837 <wilto837@student.liu.se>
2022-06-22 15:08:10 +0200

    added buttons

--- src/components/LayoutButtons.js
+++ src/components/LayoutButtons.js
@@ -2,19 +2,28 @@ import { Grid, Button } from '@mui/material';
 import { useState } from "react";
 
 const LayoutButtons = (props) => {
-  
+
   const [mode, setMode] = useState(0);
 
   const handleClick = (mode) => {
-    props.parentCallback(mode);
+    props.lightTheme(mode);
     setMode(mode);
+  }
+
+  const changeView = (view) => {
+    props.viewMode(view);
   }
 
   return (
     <Grid container>
-      <Grid item>
-        <Button onClick={() => { handleClick(0) }} variant="contained">Light mode</Button>
-        <Button onClick={() => { handleClick(1) }} variant="contained">Dark mode</Button>
+      <Grid container>
+        <Grid item>
+          <Button onClick={() => { handleClick(0) }} variant="contained">Light mode</Button>
+          <Button onClick={() => { handleClick(1) }} variant="contained">Dark mode</Button>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Button onClick={() => { changeView(1) }} variant="contained">Change View</Button>
       </Grid>
     </Grid>
   )
