4115718402e4f46c31f82175df52a375bfa3d19f
Aliceevelina <alice.e.molin@hotmail.se>
2022-06-23 15:42:25 +0200

    changed layout to work with WAD

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import PFD from './components/PFD';
 import WAD from './components/WAD';
 import Future from './components/Future';
 import LayoutButtons from './components/LayoutButtons';
-import { Grid, Collapse, Box } from '@mui/material';
+import { Grid, Collapse, Box, Stack } from '@mui/material';
 
 import './App.css';
 import zIndex from '@mui/material/styles/zIndex';
@@ -23,25 +23,25 @@ function App() {
   }
 
   return (
-    <Grid container style={{ padding: "50px", height: "100vh", backgroundColor: "grey" }}>
-      <Grid container style={{ position: "relative"}}>
-          <div style={{backgroundColor: 'pink', height: '500px', width: '500px'}}>
-            <WAD/>
-          </div>
-        <Grid container style={{ display: "absolute", height: "100%", position: "absolute", zIndex: '1' }}>
+    <Grid container style={{padding: '50px', height: "100vh", backgroundColor: "grey" }}>
+      <Grid container>
+        <Grid container style={{position: "relative", zIndex: '0' }}>
+          <WAD />
+        </Grid>
+        <Grid container style={{padding: '15px', height: "100%", position: "absolute", zIndex: '1'}}>
           <Grid item xs={3}>
             <Grid container direction="column" justifyContent="space-between" style={{ height: "100%" }}>
-              <Grid item xs={3} style={{ height: "30%"}}>
+              <Grid item xs={3} style={{ height: "30%" }}>
                 <Collapse in={collapse}>
                   <PFD lightTheme={lightTheme} />
                 </Collapse>
               </Grid>
-              <Grid item xs={3} style={{ height: "30%", textAlign: "center"}}>
+              <Grid item xs={3} style={{ height: "30%", textAlign: "center" }}>
                 <p>Bottom Left </p>
               </Grid>
             </Grid>
           </Grid>
-          <Grid item xs={6} style={{textAlign: "center"}}>
+          <Grid item xs={6} style={{ textAlign: "center" }}>
             Middle area
           </Grid>
           <Grid item xs={3}>
@@ -55,9 +55,9 @@ function App() {
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
-      <Grid item position="absolute" bottom={0} left={0}>
+        <Grid item position="absolute" bottom={0} left={0} style={{zIndex: '3'}}>
         <LayoutButtons lightTheme={changeLightTheme} viewMode={changeViewMode} />
+      </Grid>
       </Grid>
     </Grid>
   );
