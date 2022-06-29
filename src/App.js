b4efd6ab6aeda3f29f2bb7a7a1fbaa47fa111221
wilto837 <wilto837@student.liu.se>
2022-06-29 15:09:44 +0200

    fixed app

--- src/App.js
+++ src/App.js
@@ -39,9 +39,7 @@ function App() {
   }
 
   return (
-
     <div>
-
       <Grid container style={{ padding: '50px', height: "100vh" }}>
         <Grid container>
           <Grid container style={{ position: "relative", zIndex: '0' }}>
