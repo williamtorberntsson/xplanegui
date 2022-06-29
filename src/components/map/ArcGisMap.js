d9697d2bba4cab07ed5f493ce190b19e483fb8f6
Aliceevelina <alice.e.molin@hotmail.se>
2022-06-29 10:45:21 +0200

    switched to MapView and added rotation hook

--- src/components/map/ArcGisMap.js
+++ src/components/map/ArcGisMap.js
@@ -1,55 +1,29 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { loadModules } from "esri-loader";
 
-const Map = ({ center, orientation }) => {
-  const [zoom, setZoom] = useState(15);
+const Map = ({ center, rotation }) => {
+  const [zoom, setZoom] = useState(5);
   const [coords, setCoords] = useState([15.580926012604708, 58.41157469382408]);
   const [view, setView] = useState(null);
   const mapEl = useRef();
 
   useEffect(() => {
 
-    loadModules(["esri/Map", "esri/views/SceneView"]).then(([Map, SceneView]) => {
+    loadModules(["esri/Map", "esri/views/MapView"]).then(([Map, MapView]) => {
 
       const map = new Map({
-        basemap: 'dark-gray-vector'
+        basemap: 'gray-vector'
       });
 
-      let view = new SceneView({
+      let view = new MapView({
         container: mapEl.current,
         map: map,
-        zoom: zoom
+        zoom: zoom,
       });
 
       view.when(() => {
         setView(view);
       });
-
-      function shiftCamera(deg) {
-        const camera = view.camera.clone();
-        camera.position.longitude += deg;
-        camera.heading += 90;
-        return camera;
-      }
-
-    //   document.getElementById("move").addEventListener("click", () => {
-    //     console.log("move")
-    //     view.goTo(
-    //       shiftCamera(0.1),
-    //       {
-    //         speedFactor: 1,
-    //         easing: "linear"
-    //       });
-    //   })
-
-    //   document.getElementById("tele").addEventListener("click", () => {
-    //     console.log("tele")
-    //     view.goTo({
-    //       target: [coords[0] + 1, coords[1] + 1],
-    //       heading: 100
-    //     });
-    //     setCoords([coords[0] + 1, coords[1] + 1])
-    //   })
 
     })
 
@@ -63,16 +37,13 @@ const Map = ({ center, orientation }) => {
   useEffect(() => {
     if (view && center) {
       view.center = center;
-      view.orientation = orientation;
+      view.rotation = rotation;
     }
-  }, [center, view, orientation]);
+  }, [center, view, rotation]);
 
   return (
     <div>
       <div style={{ height: 800 }} ref={mapEl} />
-      {/* <button onClick={() => setZoom(10)}>Zoom</button>
-      <button id="move">move</button>
-      <button id="tele">tele</button> */}
     </div>
 
   );
--- src/components/map/Navigation.js
+++ src/components/map/Navigation.js
@@ -4,24 +4,20 @@ import ArcGisMap from './ArcGisMap';
 
 function Nav_map() {
     const [mapCenter, setMapCenter] = useState([15.580926012604708, 58.41157469382408]);
-    const [orientation, setOrientation] = useState(0);
+    const [mapRotation, setMapRotation] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setMapCenter([mapCenter[0] - 0.0001, mapCenter[1] - 0.0001])
-            setOrientation(orientation + 5)
+            setMapCenter([mapCenter[0] - 0.1, mapCenter[1] - 0.01])
+            setMapRotation(mapRotation + 0.1)
             console.log(mapCenter)
-        }, 1000);
+        }, 10);
 
         return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
     })
 
-    const changeCenter = () => {
-        setMapCenter([mapCenter[0] + 0.001, mapCenter[1] + 0.001])
-    }
-
     return (
-        <ArcGisMap center={mapCenter} orientation={orientation} />
+        <ArcGisMap center={mapCenter} rotation={mapRotation} />
     )
 }
 
