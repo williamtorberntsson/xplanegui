f10aabdcbaaa4100e2564f9087a429a60e939055
abc <abc@123.com>
2022-07-01 13:39:31 +0200

    Added altitude values from server

--- src/components/Altimeter.js
+++ src/components/Altimeter.js
@@ -3,9 +3,8 @@ import React from "react";
 import {AltitudeMeterBlack, AltitudeMeterWhite, AltitudePointerGrey, AltitudePointerRed } from "../images";
 
 const Altimeter = (props) => {
-  let needle = 180 + ((props.altitude % 1000) * 360) / 1000;
-  //let needleSmall = (props.altitude / 10000) * 360;
-  //let pressure = 2 * props.pressure - 1980;
+  const altitude_feet = props.altitude * 3.2808399;
+  const needle = 180 + ((altitude_feet % 1000) * 360) / 1000;
 
   return (
     <span id="altimeter">
@@ -15,7 +14,7 @@ const Altimeter = (props) => {
           <img src={props.lightTheme ? AltitudePointerRed : AltitudePointerGrey} className="box" />
         </div>
         <div className="altitude" style={{color: props.lightTheme ? "white" : "black"}}>
-          <p>{props.altitude}</p>
+          <p>{altitude_feet.toFixed(0)}</p>
         </div>
       </div>
     </span>
