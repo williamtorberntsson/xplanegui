2540fca1eadef8bd67244eb88a15f15794e10abb
abc <abc@123.com>
2022-07-01 13:41:03 +0200

    Added elevation, roll and pitch data

--- xpcServer/src/server.py
+++ xpcServer/src/server.py
@@ -13,20 +13,34 @@ def plane():
         while True:
             posi = client.getPOSI()
 
+            # Network settings
+            data_freq_dref = "sim/network/dataout/network_data_rate"
+            client.sendDREF(data_freq_dref, 60) # send data 60 times/second
+
+            # Own airplane airplane values
             groundspeed_dref = "sim/flightmodel/position/groundspeed"
             indicated_airspeed_dref = "sim/flightmodel/position/indicated_airspeed"
             heading_dref = "sim/flightmodel/position/true_psi"
+            elevation_dref = "sim/flightmodel/position/elevation"
+            thete_dref =  "sim/flightmodel/position/theta"
+            phi_dref = "sim/flightmodel/position/phi"
+
             groundspeed = client.getDREF(groundspeed_dref)
             indicated_airspeed = client.getDREF(indicated_airspeed_dref)
             heading = client.getDREF(heading_dref)
-            (groundspeed[0], indicated_airspeed[0], heading[0])
+            elevation = client.getDREF(elevation_dref)
+            theta = client.getDREF(thete_dref)
+            phi = client.getDREF(phi_dref)
 
             return {
-                "longitude": posi[0],
-                "latitude": posi[1],
+                "longitude": posi[1],
+                "latitude": posi[0],
                 "groundspeed": groundspeed[0],
                 "indicated_airspeed": indicated_airspeed[0],
                 "heading": heading[0],
+                "altitude": elevation[0],
+                "pitch": theta[0],
+                "roll": phi[0]
             }
 
 
