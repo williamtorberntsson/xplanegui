bed732a0ce505f0f2d5ad6683d23c5a7a7949dd1
wilto837 <wilto837@student.liu.se>
2022-07-01 15:26:57 +0200

    tweaked design

--- src/components/Altimeter.js
+++ src/components/Altimeter.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-import {AltitudeMeterBlack, AltitudeMeterWhite, AltitudePointerGrey, AltitudePointerRed } from "../images";
+import { AltitudeMeterBlack, AltitudeMeterWhite, AltitudePointerGrey, AltitudePointerRed } from "../images";
 
 const Altimeter = (props) => {
-  const altitude_feet = props.altitude * 3.2808399;
+  const altitude_feet = props.altitude * 3.2808399; // m to feet
   const needle = 180 + ((altitude_feet % 1000) * 360) / 1000;
+  const alt_thousand = (Math.floor(altitude_feet / 1000)).toFixed(0)
+  const alt_hundred = (altitude_feet % 1000).toFixed(0)
 
   return (
     <span id="altimeter">
@@ -13,8 +15,9 @@ const Altimeter = (props) => {
         <div className="needle box" style={{ transform: `translate(${50 * Math.cos((needle + 90) * (Math.PI / 180))}px, ${50 * Math.sin((needle + 90) * (Math.PI / 180))}px) rotate(${needle}deg) scale(0.4)` }}>
           <img src={props.lightTheme ? AltitudePointerRed : AltitudePointerGrey} className="box" />
         </div>
-        <div className="altitude" style={{color: props.lightTheme ? "white" : "black"}}>
-          <p>{altitude_feet.toFixed(0)}</p>
+        <div className="altitude" style={{ color: props.lightTheme ? "white" : "black" }}>
+          <span className="thousand" style={{ fontSize: "25px" }}>{alt_thousand}</span>
+          <span className="hundred">{('00' + alt_hundred).substring(alt_hundred.length + 2 - 3)}</span> {/*always display 3 numbers for hundred value*/}
         </div>
       </div>
     </span>
