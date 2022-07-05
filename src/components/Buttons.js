b0c8a72756e54c332e3dcb3478ae3d5f9deb728f
Aliceevelina <alice.e.molin@hotmail.se>
2022-07-05 10:10:57 +0200

    implemented visual WAD frame and buttons to change the grid layout

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,53 +1,93 @@
 import { Grid, Button } from '@mui/material';
 import { useState } from "react";
 
-const WidgetButtons = (props) => {
+const BoxButtons = (props) => {
 
-  const [mode, setMode] = useState(0);
+  // const [box, setBox] = useState(0);
 
-  const handleClick = (mode) => {
-    props.lightTheme(mode);
-    setMode(mode);
+  // const handleClick = (mode) => {
+  //   props.lightTheme(mode);
+  //   setMode(mode);
+  // }
+
+   const handleClick = (box) => {
+    props.activeBox(box);
+    // props.activeBox(box)
+    // setBox(box);
   }
 
-  const changeView = (view) => {
-    props.viewMode(view);
-  }
+  // const changeView = (view) => {
+  //   props.viewMode(view);
+  // }
 
   return (
-    <div style={{display: 'flex', justifyContent: 'space-evenly', width: '40vw', paddingBottom: '1vh'}}>
+    <div style={{display: 'flex', justifyContent: 'space-evenly', width: '35vw', paddingBottom: '1.5vh'}}>
+      <button className="button-30" onClick={() => { handleClick(1) }} role="button">UL</button>
+      <button className="button-30" onClick={() => { handleClick(2) }} role="button">BL</button>
+      <button className="button-30" onClick={() => { handleClick(3) }} role="button">UR</button>
+      <button className="button-30" onClick={() => { handleClick(4) }} role="button">BR</button>
+    </div>
+  )
+};
+
+
+const WidgetButtons = (props) => {
+
+  // const [mode, setMode] = useState(0);
+  // const [widgetType, setWidgetType] = useState('')
+
+  // const handleClick = (mode) => {
+  //   props.lightTheme(mode);
+  //   setMode(mode);
+  // }
+
+  const handleClick = (widgetType) => {
+    props.activeWidget(widgetType);
+    // setWidgetType(widgetType)
+  }
+
+  // const changeView = (view) => {
+  //   props.viewMode(view);
+  // }
+
+  return (
+    <div style={{display: 'flex', justifyContent: 'space-evenly', width: '40vw', paddingBottom: '1.5vh'}}>
       {/* <Button onClick={() => { handleClick(0) }} variant="contained">Light mode</Button>
       <Button onClick={() => { handleClick(1) }} variant="contained">Dark mode</Button>
       <Button onClick={() => { changeView(1) }} variant="contained">Change View</Button> */}
-      <button class="button-30" onClick={() => { handleClick(0) }} role="button">PFD</button>
-      <button class="button-30" onClick={() => { handleClick(1) }} role="button">COMP</button>
-      <button class="button-30" onClick={() => { handleClick(1) }} role="button">EFIS</button>
-      <button class="button-30" onClick={() => { changeView(1) }} role="button">None</button>
+      <button className="button-30" onClick={() => { handleClick('PFD') }} role="button">PFD</button>
+      <button className="button-30" onClick={() => { handleClick('COMP') }} role="button">COMP</button>
+      <button className="button-30" onClick={() => { handleClick('EFIS') }} role="button">EFIS</button>
+      <button className="button-30" onClick={() => { handleClick('None') }} role="button">None</button>
     </div>
   )
 };
 
-const BoxButtons = (props) => {
+const ExtendableButtons = () => {
 
-  const [mode, setMode] = useState(0);
-
-  const handleClick = (mode) => {
-    props.lightTheme(mode);
-    setMode(mode);
-  }
-
-  const changeView = (view) => {
-    props.viewMode(view);
-  }
 
   return (
-    <div style={{display: 'flex', justifyContent: 'space-evenly', width: '35vw', paddingBottom: '1vh'}}>
-      <button class="button-30" onClick={() => { handleClick(0) }} role="button">UL</button>
-      <button class="button-30" onClick={() => { handleClick(1) }} role="button">BL</button>
-      <button class="button-30" onClick={() => { changeView(1) }} role="button">UR</button>
-      <button class="button-30" onClick={() => { changeView(1) }} role="button">BR</button>
+    <div style={{display: 'flex', justifyContent: 'space-evenly', width: '100vw', paddingTop: '1.3vh'}}>
+      {/* <Button onClick={() => { handleClick(0) }} variant="contained">Light mode</Button>
+      <Button onClick={() => { handleClick(1) }} variant="contained">Dark mode</Button>
+      <Button onClick={() => { changeView(1) }} variant="contained">Change View</Button> */}
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
+      <button className="button-30" role="button">   </button>
     </div>
   )
 };
 
-export {BoxButtons, WidgetButtons};
+export {BoxButtons, WidgetButtons, ExtendableButtons};
--- src/components/WAD.js
+++ src/components/WAD.js
@@ -1,7 +1,7 @@
 import { React, useState, useEffect } from 'react';
 import PFD from './PFD';
 import Future from './Future';
-import {BoxButtons, WidgetButtons} from './Buttons';
+import {BoxButtons, WidgetButtons, ExtendableButtons} from './Buttons';
 import { Grid, Collapse, Box, Stack } from '@mui/material';
 
 import './WAD.css';
@@ -10,19 +10,74 @@ import Nav_map from './map/Navigation';
 
 function WAD() {
 
-    const [lightTheme, setLightTheme] = useState(0);
+    // const [lightTheme, setLightTheme] = useState(0);
+    const [activeBox, setActiveBox] = useState(0);
+    const [activeWidget, setActiveWidget] = useState("");
+    const [tempWidget1, setTempWidget1] = useState();
+    const [tempWidget2, setTempWidget2] = useState();
+    const [tempWidget3, setTempWidget3] = useState();
+    const [tempWidget4, setTempWidget4] = useState();
     const [collapse, setCollapse] = useState(true);
     const [mapCenter, setMapCenter] = useState([15.580926012604708, 58.41157469382408]);
     const [orientation, setOrientation] = useState(0);
 
-    const changeLightTheme = (theme) => {
-        setLightTheme(theme);
-        console.log("App mode: ", theme)
+    // const changeLightTheme = (theme) => {
+    //     setLightTheme(theme);
+    //     console.log("App mode: ", theme)
+    // }
+
+    const updateWidget = (widget) => {
+        setActiveWidget(widget)
     }
 
-    const changeViewMode = (mode) => {
-        setCollapse(!collapse);
+    const updateBox = (box) => {
+        setActiveBox(box);
     }
+    
+    // console.log("Active Box: ", activeBox, "Active Widget: ", activeWidget)
+
+    const changeViewMode = (box) => {
+        // setActiveBox(box)
+            // updateBox(box);
+            // console.log("Active Widget: ", activeWidget, "Active box", activeBox)
+            // // console.log("Active Box: ", box)
+            switch(box){
+                case 1:
+                    // if (activeWidget != tempWidget1) {    
+                        switch(activeWidget){
+                            case "PFD":   return setTempWidget1(<PFD/>);
+                            case "None":   return setTempWidget1();
+                        // }
+                    }
+                case 2: 
+                    // if (activeWidget != tempWidget1) {  
+                        switch(activeWidget){
+                            case "PFD":   return setTempWidget2(<PFD/>);
+                            case "None":  return setTempWidget2();
+                        // }
+                    }
+                case 3: 
+                    // if (activeWidget != tempWidget1) {  
+                        switch(activeWidget){
+                            case "PFD":   return setTempWidget3(<PFD/>);
+                            case "None": return setTempWidget3();
+                        }
+                    // }
+                case 4: 
+                    // if (activeWidget != tempWidget1) {  
+                        switch(activeWidget){
+                            case "PFD":   return setTempWidget4(<PFD/>);
+                            case "None": return setTempWidget4();
+                        }
+                    // }
+
+                
+                    console.log("Active Widget: ", activeWidget, "Active box", box)
+            }
+            // console.log("Active Widget: ", activeWidget, "Active box", box)
+    }
+            // setActiveBox(0)
+            // setActiveWidget(0)
 
     return (
         <Grid container className="wad_frame">
@@ -36,34 +91,39 @@ function WAD() {
                     <Grid item xs={3}>
                         <Grid container direction="column" className="left_container">
                             <Grid item className="widgetbox" id="wb_one">
-                                <Collapse in={collapse}>
-                                    <PFD lightTheme={lightTheme} />
-                                </Collapse>
+                                {/* <Collapse in={collapse}>
+                                    <PFD/>
+                                </Collapse> */}
+                                {tempWidget1}
                             </Grid>
                             <Grid item className="widgetbox" id="wb_two">
-                                <p>Bottom Left </p>
+                                {tempWidget2}
                             </Grid>
                         </Grid>
                     </Grid>
                     <Grid item xs={6}>
-                        Middle area
+                   
                     </Grid>
                     <Grid item xs={3}>
                         <Grid container direction="column" className="right_container">
                             <Grid item className="widgetbox" id="wb_three">
                                 <Future />
+                                {tempWidget3}
                             </Grid>
                             <Grid item className="widgetbox" id="wb_four">
-                                <p>Bottom Right </p>
+                                {tempWidget4}
                             </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
+                <Grid item position="absolute" top={'0'} right={'0'} style={{ zIndex: '3' }}>
+                    <ExtendableButtons/>
+                </Grid>
                 <Grid item position="absolute" bottom={'0'} left={'0'} style={{ zIndex: '3' }}>
-                    <BoxButtons lightTheme={changeLightTheme} viewMode={changeViewMode} />
+                    <BoxButtons activeBox={changeViewMode}/>
                 </Grid>
                 <Grid item position="absolute" bottom={'0'} right={'0'} style={{ zIndex: '3' }}>
-                    <WidgetButtons lightTheme={changeLightTheme} viewMode={changeViewMode} />
+                    <WidgetButtons activeWidget={updateWidget}/>
                 </Grid>
             </Grid>
         </Grid>
