05310f6161be9412321627a59760e5ca2278f45b
Aliceevelina <alice.e.molin@hotmail.se>
2022-07-06 16:44:49 +0200

    cleaned up code

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,24 +1,15 @@
 import { Grid, Button } from '@mui/material';
 import { useState } from "react";
 
-const BoxButtonB = (props) => {
+const BoxButtons = (props) => {
 
   return (
-    <div style={{display: 'flex', justifyContent: 'space-evenly', width: '25vw', paddingBottom: '1.5vh'}}>
-      <button className="button-30" onClick={() => {props.UL('L'); props.BL('L')}} role="button"></button>
-    </div>
-  )
-};
-
-const BoxButtonSide = (props) => {
-
-  return (
-    <div style={{display: 'flex', flexDirection:'column', justifyContent: 'space-between', width: '2vw', height: '85vh'}}>
+    <div style={{display: 'flex', flexDirection:'column', justifyContent: 'space-between', width: '1.5vw', height: '87vh'}}>
       <button className="button-40" onClick={() => { props.U('s')}} role="button"></button>
       <button className="button-40" onClick={() => { props.U('m')}} role="button"></button>
+      <button className="button-40" onClick={() => {props.U('L'); props.B('L')}} role="button"></button>
       <button className="button-40" onClick={() => { props.B('m')}} role="button"></button>
       <button className="button-40" onClick={() => {props.B('s')}} role="button"></button>
-      {/* <button className="button-30" onClick={() => { props.UL('L')}} role="button">L</button> */}
     </div>
   )
 };
@@ -26,23 +17,12 @@ const BoxButtonSide = (props) => {
 
 const WidgetButtons = (props) => {
 
-  // const [mode, setMode] = useState(0);
-  // const [widgetType, setWidgetType] = useState('')
-
-  // const handleClick = (mode) => {
-  //   props.lightTheme(mode);
-  //   setMode(mode);
-  // }
-
   const handleClick = (widgetType) => {
     props.activeWidget(widgetType);
   }
 
   return (
     <div style={{display: 'flex', justifyContent: 'space-evenly', width: '30vw', paddingBottom: '1.5vh'}}>
-      {/* <Button onClick={() => { handleClick(0) }} variant="contained">Light mode</Button>
-      <Button onClick={() => { handleClick(1) }} variant="contained">Dark mode</Button>
-      <Button onClick={() => { changeView(1) }} variant="contained">Change View</Button> */}
       <button className="button-30" onClick={() => { handleClick('PFD') }} role="button">PFD</button>
       <button className="button-30" onClick={() => { handleClick('COMP') }} role="button">COMP</button>
       <button className="button-30" onClick={() => { handleClick('EFIS') }} role="button">EFIS</button>
@@ -56,9 +36,6 @@ const ExtendableButtons = () => {
 
   return (
     <div style={{display: 'flex', justifyContent: 'space-evenly', width: '100vw', paddingTop: '1.3vh'}}>
-      {/* <Button onClick={() => { handleClick(0) }} variant="contained">Light mode</Button>
-      <Button onClick={() => { handleClick(1) }} variant="contained">Dark mode</Button>
-      <Button onClick={() => { changeView(1) }} variant="contained">Change View</Button> */}
       <button className="button-30" role="button">   </button>
       <button className="button-30" role="button">   </button>
       <button className="button-30" role="button">   </button>
@@ -78,4 +55,4 @@ const ExtendableButtons = () => {
   )
 };
 
-export {BoxButtonB, BoxButtonSide, WidgetButtons, ExtendableButtons};
+export {BoxButtons, WidgetButtons, ExtendableButtons};
--- src/components/WAD.js
+++ src/components/WAD.js
@@ -1,7 +1,7 @@
 import { React, useState, useEffect } from 'react';
 import PFD from './PFD';
 import Future from './Future';
-import { BoxButtonB, BoxButtonSide, WidgetButtons, ExtendableButtons } from './Buttons';
+import { BoxButtons, BoxButtonSide, WidgetButtons, ExtendableButtons } from './Buttons';
 import { Grid, Collapse, Box, Stack } from '@mui/material';
 import UpdateOfflineData from './map/UpdateOfflineData';
 
@@ -9,63 +9,33 @@ import './WAD.css';
 import zIndex from '@mui/material/styles/zIndex';
 import Nav_map from './map/Navigation';
 
-function GridTypeLeft({UL, BL}) {
-  // console.log(UL,BL)
+function GridType({U, B, container}) {
 
-  if (UL === 'L' && BL === 'L'){
+  if (U === 'L' && B === 'L'){
     return(
      <Grid container direction="column" className="left_container" id="wb_three">
       </Grid>)
   }
 
   else{
-    // setSizelU(type)
-    // setSizeB(type)
     return(
-      <Grid container direction="column" className="left_container">
-         <Grid item className={UL} id="wb_one">
+      <Grid container direction="column" className={container}>
+         <Grid item className={U} id="wb_one">
         </Grid>
-        <Grid item className={BL} id="wb_two">
-        </Grid>
-      </Grid>)
-  }
-}
-
-function GridTypeRight({UR, BR}) {
-  // console.log(UL,BL)
-
-  if (UR === 'L' && BR === 'L'){
-    return(
-     <Grid container direction="column" className="right_container" id="wb_three">
-      </Grid>)
-  }
-
-  else{
-    // setSizelU(type)
-    // setSizeB(type)
-    return(
-      <Grid container direction="column" className="right_container">
-         <Grid item className={UR} id="wb_one">
-        </Grid>
-        <Grid item className={BR} id="wb_two">
+        <Grid item className={B} id="wb_two">
         </Grid>
       </Grid>)
   }
 }
 
 function WAD() {
-
   // const [lightTheme, setLightTheme] = useState(0);
-  const [activeBox, setActiveBox] = useState(0);
   const [UL, setUL] = useState('');
   const [BL, setBL] = useState('');
   const [UR, setUR] = useState('');
   const [BR, setBR] = useState('');
   const [activeWidget, setActiveWidget] = useState("");
   const [showWidgets, setShowWidgets] = useState([]);
-  // const [collapse, setCollapse] = useState(true);
-  // const [mapCenter, setMapCenter] = useState([15.580926012604708, 58.41157469382408]);
-  // const [orientation, setOrientation] = useState(0);
   const [useXplaneData, setUseXplaneData] = useState(false);
   const [myAirPlaneData, setMyAirPlaneData] = useState();
   const [aiPlaneData, setAiPlaneData] = useState();
@@ -86,8 +56,6 @@ function WAD() {
     setShowWidgets(newWidgets);
   }
 
-  // console.log("Active widgets: ", showWidgets)
-  // console.log("Active Box: ", activeBox, "Active Widget: ", activeWidget)
 
   const changeViewMode = (box) => {
 
@@ -142,33 +110,25 @@ function WAD() {
         </Grid>
         <Grid container className="overlay_container">
           <Grid item xs={3}>
-            <GridTypeLeft UL={UL} BL={BL} />
+            <GridType U={UL} B={BL} container={'left_container'} />
           </Grid>
           <Grid item xs={6}>
-
           </Grid>
           <Grid item xs={3}>
-          <GridTypeRight  UR={UR} BR={BR}/>
+          <GridType  U={UR} B={BR} container={'right_container'} />
           </Grid>
         </Grid>
         <Grid item position="absolute" top={'0'} right={'0'} style={{ zIndex: '3' }}>
           <ExtendableButtons />
         </Grid>
-        <Grid item position="absolute" bottom={'0'} left={'0'} style={{ zIndex: '3' }}>
-          <BoxButtonB UL={setUL} BL={setBL}/>
-        </Grid>
         <Grid item position="absolute" bottom={'0'} left={'35vw'} right={'35vw'} style={{ zIndex: '3'}}>
           <WidgetButtons activeWidget={updateWidget} />
         </Grid>
         <Grid item position="absolute" left={'1vh'} style={{ zIndex: '3' }}>
-          <BoxButtonSide U={setUL} B={setBL}/>
-        </Grid>
-
-        <Grid item position="absolute" bottom={'0'} right={'0'} style={{ zIndex: '3' }}>
-          <BoxButtonB UL={setUR} BL={setBR}/>
+          <BoxButtons U={setUL} B={setBL}/>
         </Grid>
         <Grid item position="absolute" right={'1vh'} style={{ zIndex: '3' }}>
-          <BoxButtonSide U={setUR} B={setBR}/>
+          <BoxButtons U={setUR} B={setBR}/>
         </Grid>
       </Grid>
     </Grid>
