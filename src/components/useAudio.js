43743c022aa0cd95d3d773456d845e6ae9b73641
abc <abc@123.com>
2022-08-01 10:06:20 +0200

    added custom Hook to play sounds

---
+++ src/components/useAudio.js
@@ -0,0 +1,26 @@
+import { useEffect, useMemo, useState } from "react"
+
+/**
+ * Custom Hook to play audio.
+ * @param {string} filename for file 
+ * @returns {[state, function]}
+ */
+const useAudio = (filename) => {
+  const audio = useMemo(() => new Audio(require(`../assets/sound/${filename}`)), [filename]);
+  const [playing, setPlaying] = useState(false);
+
+  const play = () => {
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    }
+  }, [audio])
+  
+  return [playing, play]
+}
+export default useAudio;
