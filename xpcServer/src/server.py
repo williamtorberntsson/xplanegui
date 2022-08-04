737e54fb9b66b79f58c0e490f32fec639ac8390b
abc <abc@123.com>
2022-08-04 11:24:23 +0200

    added offline data via server

--- xpcServer/src/server.py
+++ xpcServer/src/server.py
@@ -1,4 +1,5 @@
 from distutils.log import warn
+from termios import ONLCR
 from turtle import pos
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
@@ -93,145 +94,200 @@ def handle_setup(data):
 
 
 @socketio.on("plane")
-def handle_plane(message):
-    """with xpc.XPlaneConnect() as client:
+def handle_plane(data):
+    if(data["online"]):
+        with xpc.XPlaneConnect() as client:
 
-        # Own airplane airplane values
-        posi = client.getPOSI()
-        groundspeed = client.getDREF(groundspeed_dref)
-        true_airspeed = client.getDREF(true_airspeed_dref)
-        alpha = client.getDREF(alpha_dref)
+            # Own airplane airplane values
+            posi = client.getPOSI()
+            groundspeed = client.getDREF(groundspeed_dref)
+            true_airspeed = client.getDREF(true_airspeed_dref)
+            alpha = client.getDREF(alpha_dref)
 
-        global nr_of_planes
-    """
+            global nr_of_planes
 
-    emit("plane", {
-        "longitude": 15.88092,
-        "latitude": 58.41157469382408,
-        "groundspeed": 10,
-        "true_airspeed": 30,
-        "true_heading": 30,
-        "altitude": 1000,
-        "pitch": 3,
-        "roll": 2,
-        "alpha": 0.4
-    })
-
-
-@socketio.on('pfd')
-def handle_pfd(message):
-    with xpc.XPlaneConnect() as client:
-
-        # Own airplane airplane values
-        posi = client.getPOSI()
-        groundspeed = client.getDREF(groundspeed_dref)
-        true_airspeed = client.getDREF(true_airspeed_dref)
-        alpha = client.getDREF(alpha_dref)
-        g = client.getDREFs(g_drefs)
-
-        emit("pfd", {
+            emit("plane", {
+            "longitude": posi[1],
+            "latitude": posi[0],
             "groundspeed": groundspeed[0],
             "true_airspeed": true_airspeed[0],
             "true_heading": posi[5],
             "altitude": posi[2],
             "pitch": posi[3],
             "roll": posi[4],
-            "alpha": alpha[0],
-            "g_nrml": g[0][0],
-            "g_axil": g[1][0],
-            "g_side": g[2][0]
+            "alpha": alpha[0]
+            })
+    else:
+        emit("plane", {
+            "longitude": 15.88092,
+            "latitude": 58.41157469382408,
+            "groundspeed": 10,
+            "true_airspeed": 30,
+            "true_heading": 30,
+            "altitude": 1000,
+            "pitch": 3,
+            "roll": 2,
+            "alpha": 0.4
+        })
+
+
+
+@socketio.on('pfd')
+def handle_pfd(data):
+    if(data["online"]):
+        with xpc.XPlaneConnect() as client:
+
+            # Own airplane airplane values
+            posi = client.getPOSI()
+            groundspeed = client.getDREF(groundspeed_dref)
+            true_airspeed = client.getDREF(true_airspeed_dref)
+            alpha = client.getDREF(alpha_dref)
+            g = client.getDREFs(g_drefs)
+
+            emit("pfd", {
+                "groundspeed": groundspeed[0],
+                "true_airspeed": true_airspeed[0],
+                "true_heading": posi[5],
+                "altitude": posi[2],
+                "pitch": posi[3],
+                "roll": posi[4],
+                "alpha": alpha[0],
+                "g_nrml": g[0][0],
+                "g_axil": g[1][0],
+                "g_side": g[2][0]
+            })
+    else:
+        emit("pfd", {
+            "groundspeed": 30,
+            "true_airspeed": 35,
+            "true_heading": 40,
+            "altitude": 4000,
+            "pitch": 3,
+            "roll": 10,
+            "alpha": 0.2,
+            "g_nrml": 1,
+            "g_axil": 0,
+            "g_side": 0
         })
 
 
 @socketio.on("weights")
-def handle_weights(message):
-    with xpc.XPlaneConnect() as client:
+def handle_weights(data):
+    if(data["online"]):
+        with xpc.XPlaneConnect() as client:
 
-        # While True:
-        # Own airplane airplane values
-        weights = client.getDREFs(weight_drefs)
+            # While True:
+            # Own airplane airplane values
+            weights = client.getDREFs(weight_drefs)
 
+            emit("weights", {
+                "fuel1": weights[0][0],
+                "fuel2": weights[1][0],
+                "total_fuel": weights[2][0],
+                "payload": weights[3][0],
+                "total": weights[4][0]
+            })
+    else:
         emit("weights", {
-            "fuel1": weights[0][0],
-            "fuel2": weights[1][0],
-            "total_fuel": weights[2][0],
-            "payload": weights[3][0],
-            "total": weights[4][0]
+            "fuel1": 1200,
+            "fuel2": 700,
+            "total_fuel": 1900,
+            "payload": 1000,
+            "total": 3000
         })
 
 
 @socketio.on("status")
-def handle_status(message):
-    with xpc.XPlaneConnect() as client:
+def handle_status(data):
+    if(data["online"]):
+        with xpc.XPlaneConnect() as client:
 
-        # While True:
-        # Own airplane airplane values
-        speedbrake = client.getDREF(speedbrake_dref)
-        outside_air_temp = client.getDREF(outside_air_temp_dref)
+            # While True:
+            # Own airplane airplane values
+            speedbrake = client.getDREF(speedbrake_dref)
+            outside_air_temp = client.getDREF(outside_air_temp_dref)
 
+            emit("status", {
+                "temperature": {
+                    "outside": outside_air_temp[0]
+                },
+                "speedbrake": speedbrake[0]
+            })
+    else:
         emit("status", {
             "temperature": {
-                "outside": outside_air_temp[0]
+                "outside": 24
             },
-            "speedbrake": speedbrake[0]
+            "speedbrake": 0
         })
 
 
 @socketio.on("warnings")
-def handle_warnings(message):
-    with xpc.XPlaneConnect() as client:
+def handle_warnings(data):
+    if(data["online"]):
+        with xpc.XPlaneConnect() as client:
 
-        # While True:
-        # Own airplane airplane values
-        warnings = client.getDREFs(warning_drefs)
+            # While True:
+            # Own airplane airplane values
+            warnings = client.getDREFs(warning_drefs)
 
+            emit("warnings", {
+                "ice": warnings[0][0],
+                "transonic": warnings[1][0]
+            })
+    else:
         emit("warnings", {
-            "ice": warnings[0][0],
-            "transonic": warnings[1][0]
+            "ice": 1,
+            "transonic": 0
         })
 
 
 @socketio.on("aiplanes")
-def handle_aiplanes(nr_ai):
-    """with xpc.XPlaneConnect() as client:
+def handle_aiplanes(data):
+    if(data["online"]):
+        with xpc.XPlaneConnect() as client:
 
-        planes_lat = client.getDREFs(planes_lat_drefs[0:nr_ai])
-        planes_lon = client.getDREFs(planes_lon_drefs[0:nr_ai])
-        planes_altitude = client.getDREFs(planes_el_drefs[0:nr_ai])
-        planes_heading = client.getDREFs(planes_heading_drefs[0:nr_ai])
+            nr_ai = data["nr_ai"]
 
-        team_status = client.getDREF(team_status_dref)
+            planes_lat = client.getDREFs(planes_lat_drefs[0:nr_ai])
+            planes_lon = client.getDREFs(planes_lon_drefs[0:nr_ai])
+            planes_altitude = client.getDREFs(planes_el_drefs[0:nr_ai])
+            planes_heading = client.getDREFs(planes_heading_drefs[0:nr_ai])
 
-        for i in range(nr_ai):
-            planes_data[i] = {
-                "longitude": planes_lon[i],
-                "latitude": planes_lat[i],
-                "true_heading": planes_heading[i],
-                "altitude": planes_altitude[i],
-                # team_status have 20 slots, the first one is always 1.0 (friendly), possible to be ourselves
-                "team_status": team_status[i+1]
-            }
-    """
+            team_status = client.getDREF(team_status_dref)
 
-    planes_data = [{
-        "longitude": 16,
-        "latitude": 59,
-        "true_heading": 70,
-        "altitude": 2000,
-        "team_status": 1
-    },
-    {
-        "longitude": 16.5,
-        "latitude": 59.2,
-        "true_heading": 140,
-        "altitude": 1000,
-        "team_status": 2
-    }
-    ]
+            for i in range(nr_ai):
+                planes_data[i] = {
+                    "longitude": planes_lon[i],
+                    "latitude": planes_lat[i],
+                    "true_heading": planes_heading[i],
+                    "altitude": planes_altitude[i],
+                    # team_status have 20 slots, the first one is always 1.0 (friendly), possible to be ourselves
+                    "team_status": team_status[i+1]
+                }
 
-    emit("aiplanes", {
-        "planes": planes_data
-    })
+            emit("aiplanes", {
+                "planes": planes_data[0:nr_ai]
+            })
+    else:
+        planes_data = [{
+            "longitude": 16,
+            "latitude": 59,
+            "true_heading": 70,
+            "altitude": 2000,
+            "team_status": 1
+        },
+        {
+            "longitude": 16.5,
+            "latitude": 59.2,
+            "true_heading": 140,
+            "altitude": 1000,
+            "team_status": 2
+        }]
+
+        emit("aiplanes", {
+            "planes": planes_data
+        })
 
 
 if __name__ == "__main__":
