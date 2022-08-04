b81086d1287d250090c98028494ace89112739a6
abc <abc@123.com>
2022-08-04 14:34:19 +0200

    fixed alpha not showing correctly

--- src/components/widgets/pfd/Alpha.js
+++ src/components/widgets/pfd/Alpha.js
@@ -11,8 +11,7 @@ import styles from "../../styles/Alpha.module.css";
 const Alpha = ({ alpha }) => {
   return (
     <div className={styles.alpha}>
-      <span>&alpha;</span>
-      <span>{alpha ? Math.abs(alpha).toFixed(0) : 0}</span>
+      <span>&alpha; {Math.abs(alpha).toFixed(1)}</span>
     </div>
   )
 }
